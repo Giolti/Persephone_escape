2da9b7bb058750c4ca77278486b86e83b3f22238
Giolti <61984154+Giolti@users.noreply.github.com>
2022-01-22 08:35:18 +0100

    Add files via upload

--- sprites/tilemap/tileset cave.tsx
+++ sprites/tilemap/tileset cave.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="tileset cave" tilewidth="48" tileheight="48" tilecount="143" columns="11">
- <image source="tileset cave.png" width="528" height="624"/>
+<tileset version="1.5" tiledversion="1.7.2" name="tileset cave" tilewidth="48" tileheight="48" tilecount="403" columns="31">
+ <image source="tileset cave.png" width="1488" height="624"/>
 </tileset>
